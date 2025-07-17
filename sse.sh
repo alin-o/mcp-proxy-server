c336a0d5a28c6650b17f49ae91a22dfa5d0c1523
Alin Olteanu <alin@regunsoft.ro>
2025-07-17 18:25:40 +0300

    shell script

---
+++ sse.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+export DOCKER_CONTAINER=0
+export JWT_SECRET="jbGllbnRJZCI6InRlc3QtY2xpZWntIiwiaWF0IjoxNzQzMjYwOTY5LCJleHAiOj"
+export KEEP_SERVER_OPEN=1
+export PUPPETEER_ARGS='{"defaultViewport": {"width": 1280, "height": 1080}, "args": ["--no-sandbox", "--no-zygote"], "headless": false, "timeout": 60000}'
+export CHROME_DEVEL_SANDBOX='/opt/google/chrome/chrome-sandbox'
+export DISPLAY=':0'
+export SCREENSHOT_SAVE_PATH="/home/alin/ai-workspace/screenshots"
+
+tsx build/sse
+# node build/generate-token.js -c test-client -e 1y
